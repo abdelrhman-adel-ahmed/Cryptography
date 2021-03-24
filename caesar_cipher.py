e0034de8b7c7532e4456bdbab71bd64e618add93
abdelrhman-adel-ahmed <abdelrhman.bedo007@gmail.com>
2021-03-24 20:09:50 +0200

    Update and rename caesar_cipher.c++ to caesar_cipher.py

---
+++ caesar_cipher.py
@@ -0,0 +1,21 @@
+
+def cipher_encryption(word, key):
+    encrypted_message = ""
+    for i in word:
+        letter = ord(i) - ord("a") + 1
+        encrypted_letter = (letter + key) % 26
+        s = chr(encrypted_letter + 97 - 1)
+        encrypted_message += s
+    return encrypted_message
+
+
+def cipher_decrypte(word, key):
+    decrypted_message = ""
+    for i in word:
+        letter = ord(i) - ord("a") + 1
+        decrypted_letter = (letter - key) % 26
+        decrypted_letter = chr(decrypted_letter + 97 - 1)
+        decrypted_message += decrypted_letter
+    return decrypted_message
+
+
