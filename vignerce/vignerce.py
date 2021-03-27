1c30cd24bcff6afc0d9705aa3bd6f67e7f52aec0
abdelrhman-adel-ahmed <abdelrhman.bedo007@gmail.com>
2021-03-27 14:12:04 +0200

    Create vignerce.py

---
+++ vignerce/vignerce.py
@@ -0,0 +1,32 @@
+def encrypt(word, key):
+    count = 0
+    key_length = len(key)
+    decrypted_message = ""
+    for i in word:
+        if count > key_length - 1:
+            count = 0
+        # will not add one because we start from 0 to 25
+        letter_num = ord(i) - ord("a")
+        key_letter_num = ord(key[count]) - ord("a")
+        decrypted_letter = letter_num + key_letter_num
+        decrypted_letter = chr(decrypted_letter + 97)
+        decrypted_message += decrypted_letter
+        count += 1
+    return decrypted_message
+
+
+def decrypt(word, key):
+    count = 0
+    key_length = len(key)
+    decrypted_message = ""
+    for i in word:
+        if count > key_length - 1:
+            count = 0
+        letter_num = ord(i) - ord("a")
+        key_letter_num = ord(key[count]) - ord("a")
+        decrypted_letter = letter_num - key_letter_num
+        decrypted_letter = chr(decrypted_letter + 97)
+        decrypted_message += decrypted_letter
+        count += 1
+    return decrypted_message
+
